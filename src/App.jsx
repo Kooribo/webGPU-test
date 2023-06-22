54a4fbf88f88f631ba4cf181ed04101951470543
Maximilian Ollendorff <maol6669@th-wildau.de>
2023-06-22 22:22:00 +0200

    feat: init webGPU

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import WebCanvas from "./WebCanvas";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+			<h1>WebGPU - test</h1>
+			<div className="card-full">
+				{!navigator.gpu ? (
+					<div className="support-error">
+						It seems like your current browser{" "}
+						<a
+							href="https://developer.mozilla.org/en-US/docs/Web/API/WebGPU_API#browser_compatibility"
+							target="_blank"
+							rel="noreferrer"
+						>
+							does not support WebGPU
+						</a>
+					</div>
+				) : (
+					<WebCanvas />
+				)}
 			</div>
-			<h1>WebGPU</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
